82efed3ee1b80b25cdd0d1fbbec2797a1581e4c6
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-10-29 22:42:45 +0530

    Update Book.js

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  description: String,
-  quantity: { type: Number, default: 1 }
-});
+  description: { type: String },
+  totalCopies: { type: Number, required: true, default: 1 },
+  availableCopies: { type: Number, required: true, default: 1 }
+}, { timestamps: true });
 
 module.exports = mongoose.model("Book", bookSchema);
